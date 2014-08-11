95a1f829c02288856f7a48353aae5f47170bfefe
PaulBGD <paul@paulbgd.me>
2014-08-11 08:46:50 -0500

    Let's let everyone know what license they're dealing with.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Blocks
 ======
 
 The new Blocks format. Supports faster and more scalable Minecraft Block files.
+
+Usage
+=====
--- src/main/java/me/paulbgd/blocks/BlocksLanguage.java
+++ src/main/java/me/paulbgd/blocks/BlocksLanguage.java
@@ -1,3 +1,27 @@
+/*
+ * COPYRIGHT AND PERMISSION NOTICE
+ *
+ * Copyright (c) 2014, PaulBGD, <paul@paulbgd.me>.
+ *
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright
+ * notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN
+ * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of a copyright holder shall not
+ * be used in advertising or otherwise to promote the sale, use or other dealings
+ * in this Software without prior written authorization of the copyright holder.
+ */
+
 package me.paulbgd.blocks;
 
 import java.io.File;
@@ -44,6 +68,7 @@ public class BlocksLanguage {
     public static String SAVE_COMMAND_INFO = "Saves your clipboard as a .blocks file";
     public static String LOAD_COMMAND_INFO = "Loads a .blocks file onto your clipboard!";
     public static String RESOURCE_COMMAND_INFO = "Loads a .blocks file from your resources onto your clipboard!";
+
     static {
         languageFile = new File(BlocksPlugin.getPluginFolder(), "language.byml"); // we're so swag, let's add a "b"
         language = YamlConfiguration.loadConfiguration(languageFile);
@@ -51,6 +76,7 @@ public class BlocksLanguage {
         reload();
         save();
     }
+
     private static YamlConfiguration language;
 
     /**
